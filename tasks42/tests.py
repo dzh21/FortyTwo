ca38a2035209df408871d1463c22db60411a12a0
dzh <dzh21@tut.by>
2014-11-11 17:32:46 +0300

    ticket:1 several tests for main view

--- tasks42/tests.py
+++ tasks42/tests.py
@@ -4,19 +4,23 @@ from django.test import TestCase
 class MainViewTest(TestCase):
     fixtures = ['initial_data.json']
 
-    def test_root_url_for_contacts(self):
-        response = self.client.get('/')
-        self.assertEquals(response.status_code, 200)
+    def setUp(self):
+        self.response = self.client.get('/')
 
-        self.assertTemplateUsed(response, 'base.html')
-        self.assertTemplateUsed(response, 'home.html')
+    def test_root_url_for_template_usage(self):
+        self.assertEquals(self.response.status_code, 200)
 
-        persons_in_context = response.context['persons']
+        self.assertTemplateUsed(self.response, 'base.html')
+        self.assertTemplateUsed(self.response, 'home.html')
+
+    def test_root_url_context_for_one_person_object(self):
+        persons_in_context = self.response.context['persons']
 
         # only one Person object in context
         self.assertEquals(len(persons_in_context), 1)
 
+    def test_root_url_content_for_my_contacts(self):
         # Person object in content
-        self.assertIn('Evhen', response.content)
-        self.assertIn('dzh21@tut.by', response.content)
-        self.assertIn('Chernigov region', response.content)
+        self.assertIn('Evhen', self.response.content)
+        self.assertIn('dzh21@tut.by', self.response.content)
+        self.assertIn('Chernigov region', self.response.content)
